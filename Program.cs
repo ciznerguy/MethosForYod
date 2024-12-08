ed58fc9b706c4d235ca25845840c3346cedc9a05
Guy  Cizner <46866327+ciznerguy@users.noreply.github.com>
2024-12-08 16:22:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethosForYod
+{
+    internal class Program
+    {
+        public static void PrintFullName (string firstName, string lastName)
+        {
+            Console.WriteLine($" full name {firstName} {lastName}");
+        }
+
+        public static void PrintShape (int Length)
+        {
+            for (int  i = 0; i<Length;  i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+        }
+        public static void SumOfTwoNumbers(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("The sum of two numbers is: " + sum);
+        }
+
+        public static int SumOfTwoNumbersReturn(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        public static int Sign(int num)
+        {
+            if (num > 0)
+            {
+                return 1;
+            }
+            else if (num < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static bool IsEven ( int num)
+        {
+            return num % 2 == 0;
+        }
+
+        public static void CheckNum(int n)
+        {
+            int sign =Sign(n); 
+        }
+        public static void Targill5047(int num)
+        {
+            if (num % 2 == 0)
+            {
+                for (int i = num; i < num + 10; i++)
+                {
+                    Console.Write(i+",");
+                }
+            }
+            else
+            {
+                for (int i = num; i > num - 10; i--)
+                {
+                    Console.Write(i + ",");
+                }
+            }
+        }
+
+
+        // יש לכתוב פעולה המקבלת שני מספרים ומחזירה אמת אם
+       //
+       //הראשון גדול מהשני
+        public static bool IsGreater(int num1, int num2)
+        {
+            return (num1 > num2);
+            
+           
+        }
+        public static void Targill5048(int num1)
+        {
+            int result = Fibonacci(num1);
+            
+                Console.WriteLine(result);
+            
+
+        }
+        public static int Fibonacci(int num)
+        {
+            int first = 1;
+            int second = 1;
+            for (int i = 1; i < num-1; i++)
+            {
+                int temp = first;
+                first = second;
+                second = temp + second;
+
+            }
+            return second;
+        }
+
+            static void Main(string[] args)
+        {
+            Targill5048(6);
+        }
+           
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MethosForYod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MethosForYod")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2253442-71cb-46f4-a2fa-9379d0bf9da7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
